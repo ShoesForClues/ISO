9b24b58b65d82e43c2eb06ace7d112fe45dd87cd
Shoelee <38192767+ShoesForClues@users.noreply.github.com>
2023-05-30 23:59:09 -0600

    Added more instructions
    Implemented INT, REG, NUM and ARR

--- src/c/vm.h
+++ src/c/vm.h
@@ -5,36 +5,37 @@
 
 #define ISO_OP_NOP 0x00
 #define ISO_OP_INT 0x10
-#define ISO_OP_RSP 0x11
+#define ISO_OP_REG 0x11
 #define ISO_OP_NUM 0x20
 #define ISO_OP_ARR 0x21
-#define ISO_OP_INC 0x30
-#define ISO_OP_DEC 0x31
-#define ISO_OP_DUP 0x32
-#define ISO_OP_POP 0x33
-#define ISO_OP_ROT 0x34
-#define ISO_OP_SET 0x40
-#define ISO_OP_GET 0x41
-#define ISO_OP_JMP 0x50
-#define ISO_OP_JEQ 0x51
-#define ISO_OP_JNE 0x52
-#define ISO_OP_JLS 0x53
-#define ISO_OP_JLE 0x54
-#define ISO_OP_ADD 0x60
-#define ISO_OP_SUB 0x61
-#define ISO_OP_MUL 0x62
-#define ISO_OP_DIV 0x63
-#define ISO_OP_POW 0x64
-#define ISO_OP_MOD 0x65
-#define ISO_OP_NOT 0x70
-#define ISO_OP_AND 0x71
-#define ISO_OP_BOR 0x72
-#define ISO_OP_XOR 0x73
-#define ISO_OP_LSH 0x74
-#define ISO_OP_RSH 0x75
+#define ISO_OP_SET 0x30
+#define ISO_OP_GET 0x31
+#define ISO_OP_INC 0x32
+#define ISO_OP_DEC 0x33
+#define ISO_OP_DUP 0x34
+#define ISO_OP_POP 0x35
+#define ISO_OP_ROT 0x36
+#define ISO_OP_JMP 0x40
+#define ISO_OP_JEQ 0x41
+#define ISO_OP_JNE 0x42
+#define ISO_OP_JLS 0x43
+#define ISO_OP_JLE 0x44
+#define ISO_OP_ADD 0x50
+#define ISO_OP_SUB 0x51
+#define ISO_OP_MUL 0x52
+#define ISO_OP_DIV 0x53
+#define ISO_OP_POW 0x54
+#define ISO_OP_MOD 0x55
+#define ISO_OP_NOT 0x60
+#define ISO_OP_AND 0x61
+#define ISO_OP_BOR 0x62
+#define ISO_OP_XOR 0x63
+#define ISO_OP_LSH 0x64
+#define ISO_OP_RSH 0x65
 
 /* Interrupt codes */
 
+#define ISO_INT_NONE                0x0000
 #define ISO_INT_ILLEGAL_INSTRUCTION 0x0100
 #define ISO_INT_ILLEGAL_JUMP        0x0101
 #define ISO_INT_END_OF_PROGRAM      0x0102
@@ -42,7 +43,14 @@
 #define ISO_INT_STACK_UNDERFLOW     0x0201
 #define ISO_INT_OUT_OF_BOUNDS       0x0202
 
+/* Register codes */
+
+#define ISO_REG_INT 0x00
+#define ISO_REG_PC  0x01
+#define ISO_REG_SP  0x02
+
 typedef double        iso_word;
+typedef signed int    iso_sint;
 typedef unsigned int  iso_uint;
 typedef unsigned char iso_char;
 
@@ -56,13 +64,17 @@ typedef struct {
 	iso_word *stack;
 } iso_vm;
 
+void iso_vm_interrupt(iso_vm *context,iso_uint interrupt) {
+	context->INT=interrupt;
+}
+
 iso_char iso_vm_fetch(iso_vm *context) {
 	iso_uint  PC           = context->PC;
 	iso_uint  program_size = context->program_size;
 	iso_char *program      = context->program;
 	
 	if (PC==program_size) {
-		context->INT=ISO_INT_END_OF_PROGRAM;
+		iso_vm_interrupt(context,ISO_INT_END_OF_PROGRAM);
 		
 		return 0;
 	}
@@ -78,7 +90,7 @@ void iso_vm_push(iso_vm *context,iso_word value) {
 	iso_word *stack      = context->stack;
 	
 	if (SP>=stack_size) {
-		context->INT=ISO_INT_STACK_OVERFLOW;
+		iso_vm_interrupt(context,ISO_INT_STACK_OVERFLOW);
 		
 		return;
 	}
@@ -92,7 +104,7 @@ iso_word iso_vm_pop(iso_vm *context) {
 	iso_word *stack = context->stack;
 	
 	if (SP==0) {
-		context->INT=ISO_INT_STACK_UNDERFLOW;
+		iso_vm_interrupt(context,ISO_INT_STACK_UNDERFLOW);
 		
 		return 0;
 	}
@@ -107,7 +119,7 @@ void iso_vm_set(iso_vm *context,iso_uint address,iso_word value) {
 	iso_word *stack = context->stack;
 	
 	if (address>=SP) {
-		context->INT=ISO_INT_OUT_OF_BOUNDS;
+		iso_vm_interrupt(context,ISO_INT_OUT_OF_BOUNDS);
 		
 		return;
 	}
@@ -120,7 +132,7 @@ iso_word iso_vm_get(iso_vm *context,iso_uint address) {
 	iso_word *stack = context->stack;
 	
 	if (address>=SP) {
-		context->INT=ISO_INT_OUT_OF_BOUNDS;
+		iso_vm_interrupt(context,ISO_INT_OUT_OF_BOUNDS);
 		
 		return 0;
 	}
@@ -130,7 +142,7 @@ iso_word iso_vm_get(iso_vm *context,iso_uint address) {
 
 void iso_vm_goto(iso_vm *context,iso_uint address) {
 	if (address>=context->program_size) {
-		context->INT=ISO_INT_ILLEGAL_JUMP;
+		iso_vm_interrupt(context,ISO_INT_ILLEGAL_JUMP);
 		
 		return;
 	}
@@ -150,37 +162,82 @@ iso_uint iso_vm_run(iso_vm *context) {
 	
 	/* Working variables */
 	
-	iso_uint A,B;
-	iso_word C,D;
+	iso_uint A,B,C,D;
+	iso_sint E,F,G,H;
+	iso_word I,J,K,L;
 	
 	do {
 		switch(iso_vm_fetch(context)) {
 			case ISO_OP_NOP:
 				break;
 			case ISO_OP_INT:
-				*INT=(iso_uint)iso_vm_fetch(context);
+				iso_vm_interrupt(
+					context,
+					(iso_uint)iso_vm_fetch(context)
+				);
 				
 				break;
-			case ISO_OP_RSP:
-				iso_vm_push(context,(iso_word)*SP);
+			case ISO_OP_REG:
+				switch(iso_vm_fetch(context)) {
+					case ISO_REG_INT:
+						iso_vm_push(context,(iso_word)*INT);
+						
+						break;
+					case ISO_REG_PC:
+						iso_vm_push(context,(iso_word)*PC);
+						
+						break;
+					case ISO_REG_SP:
+						iso_vm_push(context,(iso_word)*SP);
+						
+						break;
+					default:
+						iso_vm_interrupt(
+							context,
+							ISO_INT_ILLEGAL_INSTRUCTION
+						);
+				}
 				
 				break;
 			case ISO_OP_NUM:
-				A = 0;
+				B = (iso_uint)iso_vm_fetch(context);
+				C = 0;
 				
-				for (iso_char i=0; i<iso_vm_fetch(context); i+=1)
-					A = (A<<8)|(iso_uint)iso_vm_fetch(context);
+				for (A=0; A<B; A++) {
+					D = (iso_uint)iso_vm_fetch(context);
+					C = (C<<8)|D;
+				}
 				
-				iso_vm_push(context,(iso_word)A);
+				iso_vm_push(context,(iso_word)C);
+				
+				break;
+			case ISO_OP_ARR:
+				E = (iso_sint)iso_vm_fetch(context);
+				F = (iso_sint)iso_vm_fetch(context);
+				
+				for (G=0; G<F; G++) {
+					A = 0;
+					
+					for (H=0; H<E; H++) {
+						B = (iso_uint)iso_vm_fetch(context);
+						A = (A<<8)|B;
+					}
+					
+					iso_vm_push(context,(iso_word)A);
+				}
 				
 				break;
 			case ISO_OP_INC:
-				for (iso_uint i=0; i<(iso_uint)iso_vm_pop(context); i++)
+				B = (iso_uint)iso_vm_pop(context);
+				
+				for (A=0; A<B; A++)
 					iso_vm_push(context,0);
 				
 				break;
 			case ISO_OP_DEC:
-				for (iso_uint i=0; i<(iso_uint)iso_vm_pop(context); i++)
+				B = (iso_uint)iso_vm_pop(context);
+				
+				for (A=0; A<B; A++)
 					iso_vm_pop(context);
 				
 				break;
@@ -199,11 +256,11 @@ iso_uint iso_vm_run(iso_vm *context) {
 				if (*SP<2)
 					break;
 				
-				C = iso_vm_get(context,*SP-2);
-				D = iso_vm_get(context,*SP-1);
+				I = iso_vm_get(context,*SP-2);
+				J = iso_vm_get(context,*SP-1);
 				
-				iso_vm_set(context,*SP-2,D);
-				iso_vm_set(context,*SP-1,C);
+				iso_vm_set(context,*SP-2,J);
+				iso_vm_set(context,*SP-1,I);
 				
 				break;
 			case ISO_OP_JMP:
@@ -214,66 +271,77 @@ iso_uint iso_vm_run(iso_vm *context) {
 				break;
 			case ISO_OP_JEQ:
 				A = (iso_uint)iso_vm_pop(context);
-				D = iso_vm_pop(context);
-				D = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				if (C==D)
+				if (I==J)
 					iso_vm_goto(context,A);
 				
 				break;
 			case ISO_OP_JNE:
 				A = (iso_uint)iso_vm_pop(context);
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				if (C!=D)
+				if (I!=J)
 					iso_vm_goto(context,A);
 				
 				break;
 			case ISO_OP_JLS:
 				A = (iso_uint)iso_vm_pop(context);
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				if (C<D)
+				if (I<J)
 					iso_vm_goto(context,A);
 				
 				break;
 			case ISO_OP_JLE:
 				A = (iso_uint)iso_vm_pop(context);
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				if (C<=D)
+				if (I<=J)
 					iso_vm_goto(context,A);
 				
 				break;
 			case ISO_OP_ADD:
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				iso_vm_push(context,C+D);
+				iso_vm_push(context,I+J);
 				
 				break;
 			case ISO_OP_SUB:
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				iso_vm_push(context,C-D);
+				iso_vm_push(context,I-J);
 				
 				break;
 			case ISO_OP_MUL:
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				iso_vm_push(context,C*D);
+				iso_vm_push(context,I*J);
 				
 				break;
 			case ISO_OP_DIV:
-				D = iso_vm_pop(context);
-				C = iso_vm_pop(context);
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
 				
-				iso_vm_push(context,C/D);
+				iso_vm_push(context,I/J);
+				
+				break;
+			case ISO_OP_POW: /* NYI */
+				
+				break;
+			case ISO_OP_MOD:
+				J = iso_vm_pop(context);
+				I = iso_vm_pop(context);
+				E = (iso_sint)(I/J);
+				
+				iso_vm_push(context,I-(iso_word)E*J);
 				
 				break;
 			case ISO_OP_NOT:
@@ -318,7 +386,10 @@ iso_uint iso_vm_run(iso_vm *context) {
 				
 				break;
 			default:
-				*INT=ISO_INT_ILLEGAL_INSTRUCTION;
+				iso_vm_interrupt(
+					context,
+					ISO_INT_ILLEGAL_INSTRUCTION
+				);
 		}
 	} while (*INT==0);
 	
